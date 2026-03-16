f71a73649d909a4947bd659c5cea251146ac162b
Carlos <c.amigo@cesjuanpablosegundocadiz.es>
2026-03-16 20:40:47 +0100

    Primer Commit

---
+++ api/paquetes.php
@@ -0,0 +1,10 @@
+<?php
+require 'db.php';
+
+try {
+    $stmt = $pdo->query("SELECT * FROM paquetes ORDER BY nombre");
+    echo json_encode($stmt->fetchAll());
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Error en la base de datos']);
+}
